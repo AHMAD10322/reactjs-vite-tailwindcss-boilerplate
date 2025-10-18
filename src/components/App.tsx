6080a4a8cfec352d4f8c5e9f2dd0f9ec6052c99e
Mia Ahmad <miaahmad2022@gmail.com>
2025-10-18 13:51:11 +0500

    changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Ahmad</h1>
+  return (
+    <h1 className="flex h-screen w-screen items-center justify-center bg-gray-400 text-gray-800">
+      <h1>Hello world</h1>
+    </h1>
+  )
 }
 
 export default App
